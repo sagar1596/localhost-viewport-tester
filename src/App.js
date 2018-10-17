06e1e931aacd729565ad6e0541da5fc4cce7049f
Sagar Bhat <sabhat@BLR.ALLEGISINDIA.COM>
2018-10-17 16:56:52 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import {InputComponent} from './components/input-component/input-component'
+
+class App extends Component {
+  render() {
+    return (
+      <InputComponent />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/input-component/input-component.js
@@ -0,0 +1,49 @@
+import React, { Component} from 'react';
+
+export class InputComponent extends Component {
+    handleClick = (e) =>  {
+        const url = document.getElementById('myURL'),
+        width = document.getElementById('myWidth'),
+        height = document.getElementById('myHeight'),
+        iframe = document.createElement('iframe');
+        iframe.src =  url.value !== '' ? url.value : 'http://localhost:3001';
+        iframe.height = height.value !== '' ? height.value : '600px';
+        iframe.width = width.value !== '' ? width.value : '320px';
+        document.getElementsByClassName('content')[0].appendChild(iframe);
+        e.preventDefault();
+    }
+    handleScroll() {
+        alert('yes');
+        debugger;
+    }
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+              <img class="logo" alt="Logo" src="/logo.png" />
+              <input type="text" placeholder="URL" id="myURL" />
+              <input type="text" placeholder="Width" id="myWidth" />
+              <input type="text" placeholder="Height" id="myHeight" />
+              <a
+                className="App-link"
+                onClick={this.handleClick}
+                href="/"
+                rel="noopener noreferrer"
+              >
+                Open
+              </a>
+            </header>
+            <div className="content" id="contentDiv" ref='wrapper' >
+
+            </div>
+          </div>
+        )
+    } 
+
+    
+    componentDidMount() {
+    }
+    
+    componentWillUnmount() {
+    }
+}
